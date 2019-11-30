b4101d26526baeb2e1b6bd55e8bdb8b8a4e942c9
Jaccob Kii <jaccobkii@gmail.com>
2019-12-01 06:32:50 +1100

    page

--- src/components/viewers/GalleryViewer/GalleryViewer.tsx
+++ src/components/viewers/GalleryViewer/GalleryViewer.tsx
@@ -4,52 +4,62 @@ import { ZHTItemViewerOptions } from '../../../types';
 import { Grid, Button } from '@material-ui/core';
 import { loadCachedFile } from '../../../actions';
 import { arrayToDataUrl, getExt } from './utils';
+import { RouteComponentProps, Route } from 'react-router';
+import { zhtHistory } from '../../routes';
 
 const imgStyle: CSSProperties = {
     maxWidth: '100%'
 }
 
 export const GalleryViewer = ({item, fileMap}: ZHTItemViewerOptions<GalleryMeta>) => {
-    useEffect(() => {
-        loadPage(0)
-    }, [])
-    const [page, setPage] = useState(0)
-    const [dataURL, setDataURL] = useState<string | null>(null)
-    const hasNextPage = () => page < item.meta.pageNumber - 1
-    const hasPreviousPage = () => page > 0
-    async function loadPage(p: number){
-        setDataURL(null)
-        const name = item.meta.files[page]
-        const data = await loadCachedFile(name, item, fileMap)
-        setDataURL(await arrayToDataUrl(data, getExt(name)))
-        setPage(p)
-    }
-    async function nextPage(){
-        if(!hasNextPage()) return;
-        loadPage(page+1)
-    }
-    async function previousPage(){
-        if(!hasPreviousPage()) return;
-        loadPage(page-1)
+    const Page = (props: RouteComponentProps<{page?: string}>) => {
+        const page = props.match.params.page ? parseInt(props.match.params.page) : 0
+        const [dataURL, setDataURL] = useState<string | null>(null)
+        const hasNextPage = () => page < item.meta.pageNumber - 1
+        const hasPreviousPage = () => page > 0
+        async function nextPage(){
+            if(!hasNextPage()) return;
+            zhtHistory.push(`/view/${item.id}/${page+1}`)
+        }
+        async function previousPage(){
+            if(!hasPreviousPage()) return;
+            zhtHistory.push(`/view/${item.id}/${page-1}`)
+        }
+
+        async function loadPage(){
+            const name = item.meta.files[page]
+            const data = await loadCachedFile(name, item, fileMap)
+            const dataURL = await arrayToDataUrl(data, getExt(name))
+            setDataURL(dataURL)
+        }
+
+        useEffect(() => {
+            loadPage()
+        }, [])
+
+        return <Grid container>
+            <Grid item xs={12}>
+                {dataURL ? <img style={imgStyle} src={dataURL}/> : <div>Loading...</div>}
+            </Grid>
+            <Grid item xs={6}>
+                <Button
+                    fullWidth
+                    onClick={previousPage}
+                    disabled={!hasPreviousPage()}
+                    >Previous Page</Button>
+            </Grid>
+            <Grid item xs={6}>
+                <Button
+                    fullWidth
+                    onClick={nextPage}
+                    disabled={!hasNextPage()}
+                    >Next Page</Button>
+            </Grid>
+        </Grid>
     }
 
-    return <Grid container>
-        <Grid item xs={12}>
-            {dataURL ? <img style={imgStyle} src={dataURL}/> : <div>Loading...</div>}
-        </Grid>
-        <Grid item xs={6}>
-            <Button
-                fullWidth
-                onClick={previousPage}
-                disabled={!hasPreviousPage()}
-                >Previous Page</Button>
-        </Grid>
-        <Grid item xs={6}>
-            <Button
-                fullWidth
-                onClick={nextPage}
-                disabled={!hasNextPage()}
-                >Next Page</Button>
-        </Grid>
-    </Grid>
+    return <Route>
+        <Route path="/view/:id" exact component={Page}/>
+        <Route path="/view/:id/:page" exact component={Page}/>
+    </Route>
 }
